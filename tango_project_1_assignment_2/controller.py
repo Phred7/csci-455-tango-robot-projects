6e8e12dfe836fa3c468dcdc553053af09401184c
wward <woward00@gmail.com>
2022-03-01 18:14:34 -0700

    add main

--- tango_project_1_assignment_2/controller.py
+++ tango_project_1_assignment_2/controller.py
@@ -6,24 +6,25 @@ import serial
 class Controller:
 
     def __init__(self) -> None:
-        servo_robot_anatomy_map: Dict[str: int] = {"motors": 0x01,
-                                                   "waist": 0x02,
-                                                   "head_pan": 0x03,
-                                                   "head_tilt": 0x04,
-                                                   "r_shoulder": 0x05,
-                                                   "r_bicep": 0x06,
-                                                   "r_elbow": 0x07,
-                                                   "r_wrist_tilt": 0x08,
-                                                   "r_wrist_pan": 0x09,
-                                                   "r_grip": 0x10,
-                                                   "l_shoulder": 0x12,
-                                                   "l_bicep": 0x13,
-                                                   "l_elbow": 0x14,
-                                                   "l_wrist_tilt": 0x15,
-                                                   "l_wrist_pan": 0x16,
-                                                   "l_grip": 0x17
-                                                   }
-        self.servo_robot_anatomy_map: Dict[str: int] = {"motors": 0x01, "waist": "02", "head_pan": "03", "head_tilt": "04"}
+        self.servo_robot_anatomy_map: Dict[str: int] = {"motors": 0x01,
+                                                        "waist": 0x02,
+                                                        "head_pan": 0x03,
+                                                        "head_tilt": 0x04,
+                                                        "r_shoulder": 0x05,
+                                                        "r_bicep": 0x06,
+                                                        "r_elbow": 0x07,
+                                                        "r_wrist_tilt": 0x08,
+                                                        "r_wrist_pan": 0x09,
+                                                        "r_grip": 0x10,
+                                                        "l_shoulder": 0x12,
+                                                        "l_bicep": 0x13,
+                                                        "l_elbow": 0x14,
+                                                        "l_wrist_tilt": 0x15,
+                                                        "l_wrist_pan": 0x16,
+                                                        "l_grip": 0x17
+                                                        }
+        self.servo_robot_anatomy_map: Dict[str: int] = {"motors": 0x01, "waist": "02", "head_pan": "03",
+                                                        "head_tilt": "04"}
         self.servo_controller = None
         try:
             self.servo_controller = serial.Serial('/dev/ttyACM0')
@@ -37,5 +38,11 @@ class Controller:
     def drive_servo(self, servo: str, target: int) -> None:
         lsb = target & 0x7F
         msb = (target >> 7) & 0x7F
-        serial_command = chr(0xaa) + chr(0xC) + chr(0x04) + chr(self.servo_robot_anatomy_map.get(servo)) + chr(lsb) + chr(msb)
+        serial_command = chr(0xaa) + chr(0xC) + chr(0x04) + chr(self.servo_robot_anatomy_map.get(servo)) + chr(
+            lsb) + chr(msb)
         self.servo_controller.write(serial_command)
+
+
+if __name__ == '__main__':
+    controller: Controller = Controller()
+    controller.drive_servo("waist", 8000)

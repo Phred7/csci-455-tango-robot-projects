54650a3ebf6809407897fcf624720779dda8b556
wward <woward00@gmail.com>
2022-03-22 18:59:26 -0600

    Update voice_input.py

--- tango_project_2_assignment_3/voice_input.py
+++ tango_project_2_assignment_3/voice_input.py
@@ -4,8 +4,8 @@ listening = True
 while listening:
     with sr.Microphone() as source:
         r = sr.Recognizer()
-        r.adjust_for_ambient_noise(source)
-        r.dyanmic_energythreshhold = 3000
+        # r.adjust_for_ambient_noise(source)
+        # r.dyanmic_energythreshhold = 3000
 
         try:
             print("listening")

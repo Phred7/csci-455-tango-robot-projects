be6191ba5fa3dc96a5b11ff77cd3e5b2c5f0b3a5
wward <woward00@gmail.com>
2022-04-02 10:45:53 -0600

    add file to parser

--- tango_project_3_assignment_4/tango_chat_file_parser.py
+++ tango_project_3_assignment_4/tango_chat_file_parser.py
@@ -1,9 +1,13 @@
 
 class TangoChatFileParser:
 
-    def __init__(self) -> None:
+    def __init__(self, chat_file: str) -> None:
+        self.chat_file: str = chat_file
         self.__parse()
         pass
 
     def __parse(self):
+        with open(self.chat_file, 'r') as chat_file:
+            chat_file = iter(chat_file)
+            line: str = ""
         pass

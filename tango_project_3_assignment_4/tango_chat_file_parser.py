e14fa06c2dce409569ab292664a877cb1d3430b3
Justin Scarbrough <justinscarbrough0@gmail.com>
2022-04-02 12:40:05 -0600

    wordgroup parsing

--- tango_project_3_assignment_4/tango_chat_file_parser.py
+++ tango_project_3_assignment_4/tango_chat_file_parser.py
@@ -20,16 +20,41 @@ class TangoChatFileParser:
             line: str = ""
             try:
                 while not end_of_file:
-                    # line = next(chat_file)
+                    line = next(chat_file)
+                    # check errers
+                    if "#" in line:
+                        line = line[:line.index("#")]
+                    elif "~" in line:
+                        wordgrouplist = []
+                        words = line[line.index(":")+1:]
+                        if(words[0]==" "):
+                            words = words [1:]
+                        quote = False
+                        prevInd = 1
+                        for ind in range(0, len(words)):
+                            #print("Ind is:",ind,"Char is",words[ind])
+                            if words[ind] == "\"":
+                                quote = not quote
+                            elif words[ind] == " " and not quote or words[ind] == "]":
+                                #remove quotes
+                                #words.replace("\"","")
+                                #do this to the list after iterating
+                                wordgrouplist.append(words[prevInd:ind])
+                                print(wordgrouplist)
+                                prevInd = ind +1
+                        self.word_sets[line[line.index("~"):line.index(":")]] = wordgrouplist
+                    elif "$" in line:
+                        pass
+                    else:
+                        pass
                     # parser here
-                    pass
+                print(self.word_sets)
             except StopIteration:
                 end_of_file = True
-        pass
 
     def user_input(self, level, input):
         pass
 
 
 if __name__ == "__main__":
-    tcfp: TangoChatFileParser = TangoChatFileParser(chat_file="tango_chat.txt")
+    tcfp: TangoChatFileParser = TangoChatFileParser(chat_file="tildetest.txt")

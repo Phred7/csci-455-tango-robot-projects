0dee2e844115a618de0fc36877138a5658d9a8ab
cbconniebernard <cbconniebernard@gmail.com>
2022-04-19 14:30:25 -0600

    trying to make popup check boxes for selecting robot actions

--- tango_project_4_assignment_5/popups.py
+++ tango_project_4_assignment_5/popups.py
@@ -4,12 +4,25 @@ from kivy.lang import Builder
 
 Builder.load_file('popup.kv')
 
+
 class HeadPopUp(Widget):
-    pass
+    checks = []
+
+    def checkbox_click(self, instance, value, choice):
+        if value == True:
+            HeadPopUp.checks.append(choice)
+            tops = ''
+            for x in HeadPopUp.checks:
+                tops = f'{tops} {x}'
+            self.ids.output.text = f'you chose {tops}'
+        else:
+            HeadPopUp.checks.remove(choice)
+
 
 class AppBuilder(App):
     def build(self):
         return HeadPopUp()
 
-if __name__ = '__main__'
+
+if __name__ == '__main__':
     AppBuilder().run()

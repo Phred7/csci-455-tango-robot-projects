a9436ec685cde94b09247f42a3bea6473135553e
wward <woward00@gmail.com>
2022-04-19 20:35:14 -0600

    Update speech.py

--- tango_project_4_assignment_5/actions/speech.py
+++ tango_project_4_assignment_5/actions/speech.py
@@ -3,5 +3,11 @@ from tango_project_4_assignment_5.action_stategy import ActionStrategy
 
 class Speech(ActionStrategy):
 
+    def __init__(self, arg_1, arg_2, arg_n):
+        super().__init__()
+        self.arg_1 = arg_1
+        self.arg_2 = arg_2
+        self.arg_n = arg_n
+
     def execute_action(self) -> None:
         return

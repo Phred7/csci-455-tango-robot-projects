12f88e53e68525d679a4281b10a1326284f4448d
cbconniebernard <cbconniebernard@gmail.com>
2022-04-20 10:27:55 -0600

    party city with the popups (things work)

--- tango_project_4_assignment_5/main.py
+++ tango_project_4_assignment_5/main.py
@@ -1,47 +1,44 @@
 import kivy
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 
-FruitsSellected = []
-
 class Widgets(Widget):
     def btn(self):
         show_popup()
 
 class P(FloatLayout):
+    Choices = []
+
     def checkboxes_click(self, instance, value, text):
         if value:
-            print(f"Select {text}")
-            self.myvar = FruitsSellected
-            # Close
-            if text == 'FINISH':
-                print(FruitsSellected)
-                self.save()
-                App.get_running_app().stop()
-                return FruitsSellected
-
-            FruitsSellected.append(str(text))
-
+            self.Choices.append(str(text))
         else:
-            print(f"UnSelect {text}")
-            FruitsSellected.remove(str(text))
+            self.Choices.remove(str(text))
 
+    def button_press(self, idk):
+        print(self.Choices)
+        # TODO save the entered info here
+        self.Choices = []
+        print(self.Choices)
+        self.parent.parent.parent.dismiss()
+
+def show_popup():
+    content = P()
+
+    popup = Popup(title="Please select from below", content=content, size_hint=(None,None),size=(600,600))
+    # content.bind(on_press=popup.dismiss)
+    popup.open()
 
 class MyApp(App):
     def build(self):
         return Widgets()
-
-
-def show_popup():
-    show = P()
-    # layout = Checkboxes()
-    popupWindow = Popup(title="Popup Window", content=show, size_hint=(None,None),size=(600,600))
-
-    popupWindow.open()
 
 
 if __name__ == "__main__":

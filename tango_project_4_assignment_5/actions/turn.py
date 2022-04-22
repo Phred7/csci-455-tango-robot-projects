733a572b501a08e6352bd18cbde18d9ba9cf24a3
wward <woward00@gmail.com>
2022-04-22 00:57:04 -0600

    Update turn.py

--- tango_project_4_assignment_5/actions/turn.py
+++ tango_project_4_assignment_5/actions/turn.py
@@ -22,11 +22,29 @@ class Turn(ActionStrategy):
         """
         if self.turn_left_bool:
             controller.left_drive_servos()
-            controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[3]
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            controller.left_drive_servos()
+            # controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[3]
             controller.left_drive_servos()
         else:
             controller.right_drive_servos()
-            controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[1]
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            controller.right_drive_servos()
+            # controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[1]
             controller.right_drive_servos()
         time.sleep(self.turn_time)
         controller.STOPDROPANDROLL()

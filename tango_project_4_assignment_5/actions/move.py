3b605e8017179ee0a345ba11ca6fe628d66e33e7
wward <woward00@gmail.com>
2022-04-22 11:04:12 -0600

    Update move.py

--- tango_project_4_assignment_5/actions/move.py
+++ tango_project_4_assignment_5/actions/move.py
@@ -28,13 +28,41 @@ class Move(ActionStrategy):
                 f"moving {'forward' if self.move_forward_bool else 'backwards'} for {self.time_to_move}seconds {'slow' if not self.speed else 'fast'}")
         else:
             if self.move_forward_bool:
-                controller.motor_velocity_counter = 4725
+                # controller.motor_velocity_counter = 4976
                 controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+                controller.forward()
+
                 # controller.forward(controller.fivestepsofPOWER[1 if not self.speed else 0])
             elif not self.move_forward_bool:
-                controller.motor_velocity_counter = 7250
+                # controller.motor_velocity_counter = 7024
                 controller.reverse()
-                controller.reverse(controller.fivestepsofPOWER[3 if not self.speed else 4])
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+                controller.reverse()
+
+                # controller.reverse(controller.fivestepsofPOWER[3 if not self.speed else 4])
             if self.time_to_move > self.max_time:
                 self.time_to_move = self.max_time
             time.sleep(self.time_to_move)

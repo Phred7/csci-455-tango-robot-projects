d15ab9c4ef7e34d58f66658d6bbb45489aec70f1
wward <woward00@gmail.com>
2022-04-22 00:12:54 -0600

    Update kivy_module.py

--- tango_project_4_assignment_5/kivy_module.py
+++ tango_project_4_assignment_5/kivy_module.py
@@ -265,7 +265,7 @@ def play_thread(_button: Button) -> None:
         # print(connies_global_array[i][1])
         # connies_global_array[i][1] = playImg
         # print(connies_global_array[i][1])
-        print(f"running {action.action_strategy_obj.type} with image \'{playImg}\'")
+        print(f"running {action.action_strategy_obj.type} with image \'{connies_global_array[i][1]}\'")
         action.execute_action(robot_controller)
         # sleep(1)
         # connies_global_array[i][1] = currentImg

55b66f421c4134b33a13f3a33b6ad4265cbe071a
cbconniebernard <cbconniebernard@gmail.com>
2022-04-21 23:52:39 -0600

    looks ugly but works

--- tango_project_4_assignment_5/kivy_module.py
+++ tango_project_4_assignment_5/kivy_module.py
@@ -112,7 +112,7 @@ class MovePopup(FloatLayout):
 
 def show_Move(trash):
     content = MovePopup()
-    popup = Popup(title="Move Action", content=content, size_hint=(None, None), size=(600, 1000))
+    popup = Popup(title="Move Action", content=content, size_hint=(None, None), size=(1400, 600))
     popup.open()
 
 
@@ -141,7 +141,7 @@ class TurnPopup(FloatLayout):
 
 def show_Turn(trash):
     content = TurnPopup()
-    popup = Popup(title="Turn Action", content=content, size_hint=(None, None), size=(600, 1000))
+    popup = Popup(title="Turn Action", content=content, size_hint=(None, None), size=(1400, 600))
     popup.open()
 
 

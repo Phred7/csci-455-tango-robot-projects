8c6555b92a42b69cf7115e4ea700179dcd4040cf
wward <woward00@gmail.com>
2022-04-22 00:52:50 -0600

    Update turn.py

--- tango_project_4_assignment_5/actions/turn.py
+++ tango_project_4_assignment_5/actions/turn.py
@@ -21,9 +21,11 @@ class Turn(ActionStrategy):
         :return: None.
         """
         if self.turn_left_bool:
+            controller.left_drive_servos()
             controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[3]
             controller.left_drive_servos()
         else:
+            controller.right_drive_servos()
             controller.motor_turn_velocity_counter = controller.fivestepsofPOWER[1]
             controller.right_drive_servos()
         time.sleep(self.turn_time)

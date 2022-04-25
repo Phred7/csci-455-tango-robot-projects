d7d2f7c3cb20845f174e7b63f2682b1ece686dd5
cbconniebernard <cbconniebernard@gmail.com>
2022-04-25 12:38:01 -0600

    here

---
+++ final_project_gross/game_runner.py
@@ -0,0 +1,9 @@
+
+class IdRatherRipMyNailOFF:
+
+    def __int__(self):
+        self.currentCoords = self.initial_coords()
+        self.nextCoords = (0,0)
+
+    def initial_coords(self):
+        return (0,0)

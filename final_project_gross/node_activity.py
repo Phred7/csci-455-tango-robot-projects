84ce525a15fe7c6c53ead248c539de35580ad5cc
W. Ward <woward00@gmail.com>
2022-04-26 00:56:21 -0600

    add speech stuffs

--- final_project_gross/node_activity.py
+++ final_project_gross/node_activity.py
@@ -1,4 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+import random
+import speech_recognition
+import pyttsx3
 
 
 class NodeActivity(ABC):
@@ -17,3 +22,36 @@ class NodeActivity(ABC):
         """
         pass
 
+    @staticmethod
+    def say(string: str) -> None:
+        text_to_speech_engine = pyttsx3.init()
+        text_to_speech_engine.setProperty('rate', 150)
+        voices = text_to_speech_engine.getProperty('voices')
+        text_to_speech_engine.setProperty('voice', voices[2].id)
+        text_to_speech_engine.say(string)
+        text_to_speech_engine.runAndWait()
+
+    @staticmethod
+    def get_speech() -> str:
+        listening = True
+        while listening:
+            with speech_recognition.Microphone() as source:
+                speech_recognizer = speech_recognition.Recognizer()
+                speech_recognizer.adjust_for_ambient_noise(source)
+                speech_recognizer.dynamic_energy_threshold = 3000
+                # r.operation_timeout = 8
+                # r.phrase_threshold = 0.15
+                try:
+                    print("listening")
+                    audio = speech_recognizer.listen(source, timeout=8)
+                    print("got audio")
+                    user_input = speech_recognizer.recognize_google(audio)
+                    return user_input
+                except speech_recognition.UnknownValueError:
+                    print("Unknown input.")
+                    strings: List[str] = ["nope", "try again", "lol what", "what did you say?", "nani",
+                                          "unrecognizable input", "pardon", "excuse eh mwa", "sorry, I missed what you said just then"]
+                    NodeActivity.say(random.choice(strings))
+                except speech_recognition.WaitTimeoutError:
+                    print("Listen timeout exceeded.")
+                    NodeActivity.say("Listen timeout exceeded.")

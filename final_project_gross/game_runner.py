ad34a28373395e1785a4fbff5d66f8cf7cb622c9
Justin Scarbrough <justinscarbrough0@gmail.com>
2022-04-25 20:44:21 -0600

    comments for future stuff

--- final_project_gross/game_runner.py
+++ final_project_gross/game_runner.py
@@ -7,3 +7,19 @@ class IdRatherRipMyNailOFF:
 
     def initial_coords(self):
         return (0,0)
+
+
+    #battle format??
+    #robot has 400 hit points
+    #depending on difficulty place in some # of enemies
+    #battle win after all enemy die
+    #enemy has hit chance of 50%, does 1 damage on hit
+
+    #recharge format
+    #go to node, get health set to 400
+
+    #coffee format
+    #go to node, get told nearest direction of exit
+
+    #fun node
+    #random effect of either health point, damage, told which node is exit, or nothing, instakill

e6cf190ea1dd5a3e86e52bebb3e8b44d26515f98
W. Ward <woward00@gmail.com>
2022-04-26 00:01:17 -0600

    add node action strategies

---
+++ final_project_gross/node_activities/charging_station_activity.py
@@ -0,0 +1,10 @@
+from node_activity import NodeActivity
+
+
+class ChargingStationActivity(NodeActivity):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def node_activity(self) -> None:
+        pass
---
+++ final_project_gross/node_activities/fun_activity.py
@@ -0,0 +1,10 @@
+from node_activity import NodeActivity
+
+
+class FunActivity(NodeActivity):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def node_activity(self) -> None:
+        pass

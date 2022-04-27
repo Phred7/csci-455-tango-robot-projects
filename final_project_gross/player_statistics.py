a30e805625974d3889dbe6ccbf89f8aeb2b6c203
Justin Scarbrough <justinscarbrough0@gmail.com>
2022-04-27 12:51:38 -0600

    xy values in playerstats, coffee shop (theoretical) functionality

--- final_project_gross/player_statistics.py
+++ final_project_gross/player_statistics.py
@@ -8,6 +8,8 @@ class PlayerStatistics:
         self.__health: int = 400
         self.__damage_to_enemy: int = 1  # TODO: might not use this, depends on how fast we want 'battles' to happen
         self.__armor: int = 0  # TODO: might not use this, use with simple factorial equation to mitigate damage (dmg-x/100)
+        self.__current_x = 0
+        self.__current_y = 0
 
     def health(self) -> int:
         return self.__health
@@ -29,3 +31,7 @@ class PlayerStatistics:
 
     def modify_armour_class(self, new_armour_class: int) -> None:
         self.__armor = new_armour_class
+
+    def set_position(self, x,y): #could be tuple
+        self.current_x = x
+        self.current_y = y

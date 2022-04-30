212e783019cd1556d362054f83906d63b4cb40a8
cbconniebernard <cbconniebernard@gmail.com>
2022-04-29 22:01:01 -0600

    testing arm

--- final_project_gross/node_activities/start_activity.py
+++ final_project_gross/node_activities/start_activity.py
@@ -13,6 +13,8 @@ class StartActivity(NodeActivity):
         # updates kivy graphics
         with open('images/picture.txt', "w") as f:
             f.write('images/start.png')
+        self.controller_interface.right_arm_up()
+        self.controller_interface.right_arm_down()
         Speech.say("Robot starting")
         Speech.say('Navigate the maze, defeat enemies and make it to the end to win!')
 

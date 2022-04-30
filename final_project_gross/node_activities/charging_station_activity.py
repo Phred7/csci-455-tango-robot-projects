225f94e5abef269c3ccbe08c11537bcffdc33018
cbconniebernard <cbconniebernard@gmail.com>
2022-04-29 21:59:01 -0600

    movesd stuff

--- final_project_gross/node_activities/charging_station_activity.py
+++ final_project_gross/node_activities/charging_station_activity.py
@@ -9,12 +9,12 @@ class ChargingStationActivity(NodeActivity):
         super().__init__(player_statistics, controller_interface)
 
     def node_activity(self) -> None:
+        with open('images/picture.txt', "w") as f:
+            f.write('images/charging-station.png')
+
         self.player_statistics.set_health(400)  # TODO: should it recharge a random amount of health in a range that doesnt exceed some ceiling.
 
         self.controller_interface.turn_left(1)
         self.controller_interface.turn_right(1)
         Speech.say('recharged health i guess')
 
-        # updates kivy graphics
-        with open('images/picture.txt', "w") as f:
-            f.write('images/charging-station.png')

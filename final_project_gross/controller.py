11dd4db583f66829d2f52640c20455b4b15b94fd
wward <woward00@gmail.com>
2022-04-29 22:18:15 -0600

    Update controller.py

--- final_project_gross/controller.py
+++ final_project_gross/controller.py
@@ -247,7 +247,7 @@ class Controller:
             self.drive_servo("l_shoulder", self.five_steps_of_POWER[0])
         if not move_up_bool:
             self.left_arm_position = 0
-            self.drive_servo("l_shoulder", self.five_steps_of_POWER[4])
+            self.drive_servo("l_shoulder", self.five_steps_of_POWER[2])
 
     def right_arm(self, move_up_bool: bool) -> None:
         if move_up_bool:
@@ -255,7 +255,7 @@ class Controller:
             self.drive_servo("r_shoulder", self.five_steps_of_POWER[0])
         if not move_up_bool:
             self.left_arm_position = 0
-            self.drive_servo("r_shoulder", self.five_steps_of_POWER[4])
+            self.drive_servo("r_shoulder", self.five_steps_of_POWER[2])
 
 
 if __name__ == '__main__':
